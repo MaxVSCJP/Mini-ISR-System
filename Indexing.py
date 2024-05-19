aeb8a13f58a2625040e2cb69c0ad0bb5fd71f628
Maassas <maassast123@gmail.com>
2024-05-19 20:50:30 +0300

    Added Searching functionality
    The Searching method is a bit hard to explain in a commit message so ask me directly if you have any questions.

--- Indexing.py
+++ Indexing.py
@@ -1,12 +1,19 @@
 import Tokenization as token
 import os
+import json
 
-diction_list = []
+diction_dict = {}
 directoryCorpus = os.getcwd() + "\\Corpus"
 corpus = os.listdir(directoryCorpus)
 
 for file in corpus:
     with open(directoryCorpus + "\\" + file, "r",encoding= "utf-8") as f:
-        diction_list.append(token.Tokenization(f))
+        diction_dict[file] = (token.Tokenization(f))
         
+
+with open ("Inverted file.txt", "w") as IFile:
+    json.dump(diction_dict, IFile)
+
+IFile.close()
+
 token.RankedBarChart()
