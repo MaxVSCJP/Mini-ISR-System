b87783cadd0c1b91812161ae53d26eeddea54547
Alazar zerubabel <alazarzerubabel6@gmail.com>
2024-05-25 10:43:44 +0300

    Update stmmer.py

--- stmmer.py
+++ stmmer.py
@@ -116,34 +116,3 @@ def geez_stemmer(word_list):
     
     
     return stemmed_words
-
-# Example usage
-word_dic = {}
-PUNC = ["።","፤","።"] 
-num = ["፪","፫","፬","፭","ዕፀ","፮","፯","፰","፱",""]
-with open('fname.txt', 'r',encoding= "utf-8") as f:
-    for line in f:
-        words = line.lower().split(" ")
-        for word in words:
-            if word in num:
-                words.remove(word)
-            else:
-                if word:
-                    if word[-1] not in PUNC :
-                        if word  in word_dic:
-                            word_dic[word] += 1
-                        else:
-                            word_dic[word] = 1
-                    else:
-                        if word  in word_dic:
-                            word_dic[word[:-1]] += 1
-                        else:
-                            word_dic[word[:-1]] = 1
-
-sorted_dict_values = dict(sorted(word_dic.items(), key=lambda item: item[1],reverse= True)) 
-
-
-
-stemmed_words = geez_stemmer(sorted_dict_values)
-for key, value in stemmed_words.items():
-    print(key,value)
